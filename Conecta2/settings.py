67ffe39d25cae1d4dbe5de5b1b4e5733f0a45d5a
Jhonnyfasio <juan9_manuel@hotmail.com>
2022-02-11 20:30:08 -0600

    With SS

--- Conecta2/settings.py
+++ Conecta2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 ##
+import os
 import dj_database_url
 from decouple import config
 ##
@@ -133,9 +134,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/0'
+sssssss
+STATIC_ROOT = ssos.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
