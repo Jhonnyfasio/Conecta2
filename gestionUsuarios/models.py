3f442bc410941e698c27db42da2bb537324f9db5
Jhonnyfasio <juan9_manuel@hotmail.com>
2022-07-06 18:46:59 -0500

    Migrations Again

--- gestionUsuarios/models.py
+++ gestionUsuarios/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
@@ -12,20 +13,44 @@ class User(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
+    def __str__(self):
+        return "%s" %(self.name)
+
+class Category(models.Model):
+    idCategory = models.IntegerField()
+    name = models.CharField(max_length=15)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return "%s" %(self.name)
+
 class Message(models.Model):
     idMessage = models.IntegerField()
+    idUser = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     message = models.CharField(max_length=1000)
     created_at = models.DateTimeField()
 
+    def __str__(self):
+        return "%s" %(self.message)
+
 class Like(models.Model):
     idLike = models.IntegerField()
+    idUser = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     score = models.SmallIntegerField()
     created_at = models.DateTimeField()
 
+    def __str__(self):
+        return "%s" %(self.score)
+
 class Card(models.Model):
     idCard = models.IntegerField()
+    idUser = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    idCategory = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     content = models.CharField(max_length=1000)
     translation = models.CharField(max_length=1000)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
+    def __str__(self):
+        return "%s" %(self.idCard)
