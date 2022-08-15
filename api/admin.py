fd9825de21bc02f8ab7d71e056016d6604a0e724
Pedrome203 <47611247+Pedrome203@users.noreply.github.com>
2022-08-14 20:17:04 -0500

    Register Models

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import CardPost
+from .models import CardPost, User, Category, Like
 
 # Register your models here.
 
 admin.site.register(CardPost)
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Like)
