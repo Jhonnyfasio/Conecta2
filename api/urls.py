24f1e1f9bbb97a9b5070c7253fb07e1db638dd35
Pedrome203 <47611247+Pedrome203@users.noreply.github.com>
2022-08-14 22:52:44 -0500

    Commit Fix

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,9 @@ from .views import CardPostView, UserView
 
 
 urlpatterns = [
-    path('cards/', CardPostView.as_view(), name='cards_list'),
+    path('cards/', CardPostView.as_view(), name='get_cards'),
+    path('cards/<int:id_category>', CardPostView.as_view(),
+         name='get_cards_for_category'),
     path('users/', UserView.as_view(), name='users_list'),
     path('likes/', UserView.as_view(), name='users_list'),
 
