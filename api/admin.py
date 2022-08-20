68dcf715db9ad2325062ea308a92e72244f444aa
Pedrome203 <47611247+Pedrome203@users.noreply.github.com>
2022-08-20 18:39:43 -0500

    Add[Model EnglishLevel]

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import CardPost, User, Category, Like
+from .models import CardPost, EnglishLevel, User, Category, Like
 
 # Register your models here.
 
 admin.site.register(CardPost)
 admin.site.register(User)
+admin.site.register(EnglishLevel)
 admin.site.register(Category)
 admin.site.register(Like)
--- api/models.py
+++ api/models.py
@@ -3,8 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class EnglishLevel(models.Model):
+    tag = models.CharField(max_length=10)
+
+
 class User(models.Model):
     name = models.CharField(max_length=1000)
+    id_english_level = models.ForeignKey(EnglishLevel, blank=True, null=True, on_delete=models.CASCADE,
+                                         db_column='id_english_level', related_name='user_english_level')
 
 
 class Category(models.Model):
@@ -16,12 +22,12 @@ class CardPost(models.Model):
     id_user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE,
                                 db_column='id_user', related_name='card_user')
     id_category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE,
-                                    db_column='id_category', related_name='card_categoryr')
+                                    db_column='id_category', related_name='card_category')
 
 
 class Like(models.Model):
     status = models.BooleanField()
-    id_user = models.ForeignKey(User, blank=True, null=False, on_delete=models.CASCADE,
+    id_user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE,
                                 db_column='id_user', related_name='like_user')
-    id_card = models.ForeignKey(CardPost, blank=True, null=False, on_delete=models.CASCADE,
+    id_card = models.ForeignKey(CardPost, blank=True, null=True, on_delete=models.CASCADE,
                                 db_column='id_card', related_name='like_card')
