27be86d3203b08fd9d3348bc871d074a772f543e
Pedrome203 <47611247+Pedrome203@users.noreply.github.com>
2022-08-20 16:47:20 -0500

    Get Likes

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('cards/', CardPostView.as_view(), name='get_cards'),
     path('cards/<int:id_user>', CardPostView.as_view(),
          name='get_cards_for_category'),
-    path('cards/', LikeView.as_view(), name='get_cards_likes'),
+    path('likes/', LikeView.as_view(), name='get_cards_likes'),
 
 ]
