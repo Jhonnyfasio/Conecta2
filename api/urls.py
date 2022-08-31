8c686a4326f693357ba4a6fea9ea81d0508ac4f8
Pedrome203 <47611247+Pedrome203@users.noreply.github.com>
2022-08-31 10:26:34 -0500

    Add List Cards User

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-from .views import CardPostView, LikeView, SaveView
+from .views import CardPostView, LikeView, SaveView, CardsUserView
 
 
 urlpatterns = [
     path('cards/<int:id_user>', CardPostView.as_view(), name='get_cards'),
     path('cards/', CardPostView.as_view(), name='post_cards'),
-    path('likes/<int:id_user>', LikeView.as_view(), name='get_likes'),
+    path('likes/<int:id_user>/<int:id_category>',
+         LikeView.as_view(), name='get_likes'),
     path('likes/', LikeView.as_view(), name='post_likes'),
     path('saves/<int:id_user>/<int:id_category>',
          SaveView.as_view(), name='get_saves_category'),
     path('saves/', SaveView.as_view(), name='post_saves'),
+    path('create_card/<int:id_user>/<int:id_category>',
+         CardsUserView.as_view(), name='get_cards_create_category'),
+
 
 ]
--- api/views.py
+++ api/views.py
@@ -79,17 +79,6 @@ class LikeView(View):
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, id_user):
-        user = User.objects.get(id=id_user)
-        cards = list(CardPost.objects.exclude(user_id=user).annotate(isLike=Count(
-            'like_card', filter=Q(like_card__status=True, like_card__user_id=user))).filter(isLike=1).values('id', 'user_id__name', 'content', 'category_id', 'user_id', 'isLike'))
-
-        if len(cards) > 0:
-            data = {'message': 'Success', 'cards': cards}
-        else:
-            data = {'message': 'Cards not found...'}
-        return JsonResponse(data)
-
     def get(self, request, id_user, id_category):
         user = User.objects.get(id=id_user)
         category = Category.objects.get(id=id_category)
@@ -169,3 +158,17 @@ class SaveView(View):
 
     def delete(self, request):
         pass
+
+
+class CardsUserView(View):
+    def get(self, request, id_user, id_category):
+        user = User.objects.get(id=id_user)
+        category = Category.objects.get(id=id_category)
+        cards = list(CardPost.objects.filter(user_id=user).filter(category_id=category).annotate(isSave=Count(
+            'save_card', filter=Q(save_card__status=True, save_card__user_id=user))).filter(isSave=1).values('id', 'user_id__name', 'content', 'category_id', 'user_id', 'isSave'))
+
+        if len(cards) > 0:
+            data = {'message': 'SUCCESS', 'cards': cards}
+        else:
+            data = {'message': 'NO FOUND'}
+        return JsonResponse(data)
