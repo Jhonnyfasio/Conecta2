844015a31c3e43b1e2370e3fedafe17a78fcda57
Juan Balderrama <juan9_manuel@hotmail.com>
2022-08-31 00:03:31 -0500

    Folder Image2

--- Conecta2/settings_production.py
+++ Conecta2/settings_production.py
@@ -3,8 +3,21 @@ import dj_database_url
 from decouple import config
 
 
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        default=config('DATABASE_URL')
+#    )
+#}
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd68amv284hpn7n',
+        'USER': 'fyotlazppuqhna',
+        'PASSWORD': '20c467abafb5c6d6f4294068e03fcfa9d4be28ed61419cb711392c34c69dc6f3',
+        'HOST': 'ec2-34-233-157-189.compute-1.amazonaws.com',
+        'DATABASE_PORT': '5432',
+    }
 }
