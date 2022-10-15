2f70b6f2aab5dccbd78959f9eabc8fc3251d0b0c
Pedrome203 <47611247+Pedrome203@users.noreply.github.com>
2022-10-15 18:40:11 -0500

    GET USER

--- api/views.py
+++ api/views.py
@@ -68,7 +68,7 @@ class UserView(View):
         accepted = StatusFriendRequest.objects.get(id=2)
 
         friends_one = list(FriendRequest.objects.filter(
-            user_s_id=user).filter(status_id=accepted).annotate(id=F('user_r_id__id'), name=F('user_r_id__name'), image=F('user_r_id__image')))
+            user_s_id=user).filter(status_id=accepted).annotate(idTest=F('id')))
 
         # friends_two = list(FriendRequest.objects.filter(
         # user_r_id=user).filter(status_id=accepted).values('user_s_id__id', 'user_s_id__name', 'user_s_id__image'))
