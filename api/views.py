23d53f0554e89bc830ee217d7058ce848408f8b2
Pedrome203 <47611247+Pedrome203@users.noreply.github.com>
2022-10-16 14:15:13 -0500

    GET USER

--- api/views.py
+++ api/views.py
@@ -59,7 +59,7 @@ class UserView(View):
         cards = list()
         newUser = list()
         friends_result = list()
-        statusRequest = list()
+
         user = User.objects.get(id=id_user)
 
         userStalker = User.objects.get(id=id_user_stalker)
@@ -80,7 +80,7 @@ class UserView(View):
         if len(statusRequest) == 0:
             statusRequest = 0
         else:
-            statusRequest = statusRequest[0]
+            statusRequest = statusRequest[0].status
         data = {'user': newUser[0], 'status': statusRequest,
                 'cards': cards, 'friends': friends_result}
         return JsonResponse(data)
