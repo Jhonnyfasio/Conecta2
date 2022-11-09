65accc1734cfbf566867696112183fc076e65380
Jhonnyfasio <juan9_manuel@hotmail.com>
2022-11-08 18:32:15 -0600

    Custom user

--- Conecta2/settings_production.py
+++ Conecta2/settings_production.py
@@ -15,7 +15,7 @@ DATABASES = {
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd68amv284hpn7n',
-        'USER': 'fyotlazppuqhna',
+        'USER': 'ftlazppuqhna',
         'PASSWORD': '20c467abafb5c6d6f4294068e03fcfa9d4be28ed61419cb711392c34c69dc6f3',
         'HOST': 'ec2-34-233-157-189.compute-1.amazonaws.com',
         'DATABASE_PORT': '5432',
