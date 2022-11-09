0c03d77985e4d29495fab5f5eead25fd619e3b8b
Jhonnyfasio <juan9_manuel@hotmail.com>
2022-11-09 16:33:13 -0600

    Adding Username UserView

--- Conecta2/router.py
+++ Conecta2/router.py
@@ -1,7 +1,8 @@
 from gestionUsuarios.viewsets import userviewsets
 from gestionUsuarios.views import SuggestionViewSet
+from api.views import CardPostViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('suggestion', SuggestionViewSet, basename='suggestionset')
+router.register(r'suggestion', SuggestionViewSet, basename='suggestionset')
 urlpatterns = router.urls
